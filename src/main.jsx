12a9ceeb6a0e9a98e7e14e412b66b0768bba1ff8
Alamin <alamin017514@gmail.com>
2023-07-28 09:11:05 +0600

    create a login page

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
+import { RouterProvider } from 'react-router-dom'
+import AuthProvider from './providers/AuthProvider'
+import { router } from './routes/Routes'
+import { Toaster } from 'react-hot-toast'
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <AuthProvider>
+    <Toaster />
+    <RouterProvider router={router} />
+  </AuthProvider>
 )
